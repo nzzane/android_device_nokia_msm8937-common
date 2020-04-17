14f0473ed476243f544b7eb2bf75079f285a4edb
Zane <github@turtledev.nz>
2020-04-17 19:05:13 +1200

    remove wfdcommon

--- msm8937.mk
+++ msm8937.mk
@@ -404,9 +404,5 @@ PRODUCT_COPY_FILES += \
     $(VENDOR_PATH)/wifi/firmware/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(VENDOR_PATH)/wifi/firmware/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/firmware/wlan/prima/WCNSS_qcom_cfg.ini
 
-# WiFi Display
-PRODUCT_BOOT_JARS += \
-    WfdCommon
-
 # Inherit common proprietary files
 #$(call inherit-product-if-exists, vendor/xiaomi/msm8937-common/msm8937-common-vendor.mk)
